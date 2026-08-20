fa25bd3f932c74f39ce92131e38b7173f55e8568
Amar Goyal <goyalamars18@gmail.com>
2026-08-20 14:02:24 -0500

    basic printing in bluej

---
+++ Chapter1/basic-print/printing.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class printing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class printing
+{
+
+    public static void main(String[] args)
+    {
+        System.out.print("This is on line 1"); //same line print
+        System.out.println("This is on line 1, but next one will be on a new line");
+        /*
+         * The next print
+         * will be on a new line
+         */
+    }
+}
