5b2ff8dbcc4a7c22170530b4cac75e47e1f841ac
Amar Goyal <goyalamars18@gmail.com>
2026-08-21 14:12:20 -0500

    budget lesson
    basic data types and user input (starting)

--- Chapter1/basic-print/printing.java
+++ Chapter1/basic-print/printing.java
@@ -1,20 +1,66 @@
-
 /**
  * Write a description of class printing here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Scanner;
+
+record mathResults(int add, double div, int multi, int sub) {}
+
 public class printing
 {
 
     public static void main(String[] args)
     {
+
         System.out.print("This is on line 1"); //same line print
         System.out.println("This is on line 1, but next one will be on a new line");
         /*
          * The next print
          * will be on a new line
          */
+        String userInput = getUserInput();
+        
+        displayUserName(userInput);
+        
+        mathResults results = getVars();
+        
+        System.out.println("added: " + results.add());
+        System.out.println("divided: " + results.div());
+        System.out.println("subracted: " + results.sub());
+        System.out.println("multiplied: " + results.multi());
+    }
+    
+    public static String getUserInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your name ");
+        String userName = scanner.nextLine();
+        return userName;
+    }
+
+
+    public static void displayUserName(String userName)
+    {
+        System.out.println("Your name is: " + userName);
+    }
+    
+    public static mathResults getVars()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        int x = input.nextInt();
+        
+        System.out.println("Enter second number: ");
+        int y = input.nextInt();
+        
+        int a = x+y;
+        double d = (double) x/y;
+        int m = x*y;
+        int s = x-y;
+        
+        return new mathResults(a, d, m, s);
     }
 }
---
+++ Chapter1/lessons/budget/WeeklyBudget.java
@@ -0,0 +1,34 @@
+
+/**
+ * Weekly Budget
+ * Amar
+ * Last Updated: 8/21/2026
+ */
+
+import java.util.Scanner;
+public class WeeklyBudget
+{
+    // Calculate weekly lunch spending and remaining money
+    public static void main(String[] args)
+    {
+        String name;
+        double allowance, lunchPrice, totalCost, remaining;
+        int lunchesPerWeek;
+        
+        // Instantiate the Scanner object to read form the keyboard
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your naeme: ");
+        name = scan.nextLine();
+        
+        // read doubles
+        System.out.print("Enter your weekly allowance: $");
+        allowance = scan.nextDouble();
+        
+        System.out.print("Enter the price of one shcool lunch: $");
+        lunchPrice = scan.nextDouble();
+        
+        System.out.print("Enter the number of school lunches you ordered this week: ");
+        lunchesPerWeek = scan.nextInt();
+
+    }
+}
